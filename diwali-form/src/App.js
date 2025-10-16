e006adc6d1da1a2f20170723699d4bc3760b5456
Aaditya Prajapati <abhiprajapati84494@gmail.com>
2025-10-16 22:04:00 +0530

    diwali wishing update

--- diwali-form/src/App.js
+++ diwali-form/src/App.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-// App.js me
 import FormPage from "./components/FormPage";
 import WishPage from "./components/WishPage";
 
@@ -7,14 +6,18 @@ function App() {
   const [wishData, setWishData] = useState(null);
   const [showWish, setShowWish] = useState(false);
 
-  // agar link ke through data aaye to decode karo
   useEffect(() => {
     const params = new URLSearchParams(window.location.search);
     const data = params.get("data");
+
     if (data) {
-      const decoded = JSON.parse(decodeURIComponent(data));
-      setWishData(decoded);
-      setShowWish(true);
+      try {
+        const decoded = JSON.parse(decodeURIComponent(data));
+        setWishData(decoded);
+        setShowWish(true);
+      } catch (e) {
+        console.error("Invalid data in URL");
+      }
     }
   }, []);
 
